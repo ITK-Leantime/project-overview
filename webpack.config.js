a5329785e1be6a15271b27f8cc5602e419b3263a
Jeppe Krogh <kjej@aarhus.dk>
2024-10-17 15:37:51 +0200

    coding standards

--- webpack.config.js
+++ webpack.config.js
@@ -3,29 +3,29 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: {
-    'project-overview': ['./assets/project-overview.js', './assets/project-overview.css']
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'js/[name].js',
-  },
-  plugins: [
+    entry: {
+        'project-overview': ['./assets/project-overview.js', './assets/project-overview.css']
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].js',
+    },
+    plugins: [
     new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
+        $: 'jquery',
+        jQuery: 'jquery',
     }),
     new MiniCssExtractPlugin({
-      filename: 'css/[name].css'
+        filename: 'css/[name].css'
     }),
   ],
-  module: {
+module: {
     rules: [
-      {
+    {
         test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
+    },
     ],
-  },
-  mode: 'production',
+    },
+    mode: 'production',
 };
