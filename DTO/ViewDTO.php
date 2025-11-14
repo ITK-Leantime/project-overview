b5cca632d40c361e0d20d0750e7727ce4fabfd3f
Jeppe Krogh <kjej@aarhus.dk>
2025-11-14 14:25:58 +0100

    Replace fixed date strings with DateTypeEnum and make dates nullable

--- DTO/ViewDTO.php
+++ DTO/ViewDTO.php
@@ -2,27 +2,31 @@
 
 namespace Leantime\Plugins\ProjectOverview\DTO;
 
+use Leantime\Plugins\ProjectOverview\Enum\DateTypeEnum;
+
 /**
  * Data Transfer Object for a projectOverview view
  */
 readonly class ViewDTO
 {
     /**
-     * @param string|null       $title           Title of the view
-     * @param array<int>        $users           Selected user IDs
-     * @param string            $fromDate        Start date of the view
-     * @param string            $toDate          End date of the view
-     * @param array<string>     $columns         Selected columns to display
-     * @param array<int, mixed> $projectFilters  Selected project filters (project IDs)
+     * @param string|null $title Title of the view
+     * @param array<int> $users Selected user IDs
+     * @param DateTypeEnum $dateType Type of date range
+     * @param string|null $fromDate Start date of the view
+     * @param string|null $toDate End date of the view
+     * @param array<string> $columns Selected columns to display
+     * @param array<int, mixed> $projectFilters Selected project filters (project IDs)
      * @param array<int, mixed> $priorityFilters Selected priority filters (priority IDs)
-     * @param array<int, mixed> $statusFilters   Selected status filters (status IDs)
-     * @param array<string>     $customFilters   Selected custom filters (filter names)
+     * @param array<int, mixed> $statusFilters Selected status filters (status IDs)
+     * @param array<string> $customFilters Selected custom filters (filter names)
      */
     public function __construct(
         public ?string $title,
         public array $users,
-        public string $fromDate,
-        public string $toDate,
+        public DateTypeEnum $dateType,
+        public ?string $fromDate,
+        public ?string $toDate,
         public array $columns,
         public array $projectFilters = [],
         public array $priorityFilters = [],
