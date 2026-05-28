929e0dc103f4b0bbef80a6fd5b4957f367b29b95
Jeppe Krogh <kjej@aarhus.dk>
2026-05-28 11:26:48 +0200

    Fixed data list load on new view page

---
+++ Templates/partials/newViewHelp.blade.php
@@ -0,0 +1,24 @@
+<section class="new-view-tips-section">
+    <button type="button" class="new-view-tips-dismiss"
+        aria-label="{{ __('projectOverview.dismiss_tips') }}"
+        title="{{ __('projectOverview.dismiss_tips') }}">×</button>
+    <h3 class="new-view-tips-heading">{{ __('projectOverview.tips_heading') }}</h3>
+    <div class="new-view-tips">
+        <div class="new-view-tip-card">
+            <h4>{{ __('projectOverview.tip_filters_title') }}</h4>
+            <p>{{ __('projectOverview.tip_filters_body') }}</p>
+        </div>
+        <div class="new-view-tip-card">
+            <h4>{{ __('projectOverview.tip_menu_title') }}</h4>
+            <p>{{ __('projectOverview.tip_menu_body') }}</p>
+        </div>
+        <div class="new-view-tip-card">
+            <h4>{{ __('projectOverview.tip_reorder_title') }}</h4>
+            <p>{{ __('projectOverview.tip_reorder_body') }}</p>
+        </div>
+        <div class="new-view-tip-card">
+            <h4>{{ __('projectOverview.tip_save_title') }}</h4>
+            <p>{{ __('projectOverview.tip_save_body') }}</p>
+        </div>
+    </div>
+</section>
