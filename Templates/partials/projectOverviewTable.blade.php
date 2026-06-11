1e338761ad482089aeba64ea42e0c3322c25a501
Jeppe Krogh <kjej@aarhus.dk>
2026-06-11 10:06:20 +0200

    Added result count to top and bottom of view table

--- Templates/partials/projectOverviewTable.blade.php
+++ Templates/partials/projectOverviewTable.blade.php
@@ -7,6 +7,8 @@
     $nextPage = $userView['nextPage'] ?? null;
     $viewId = $userView['id'] ?? '';
     $columnCount = max(1, count($columns));
+    $total = (int) ($userView['total'] ?? count($rows));
+    $loaded = (int) ($userView['loaded'] ?? count($rows));
     $nextPageUrl = $hasMore && $nextPage !== null && $viewId !== ''
         ? '/ProjectOverview/ProjectOverview/loadViewTableRows/' . urlencode((string) $viewId)
         : null;
@@ -16,6 +18,15 @@
 @if ($viewId === '__new')
     @include('projectoverview::partials.newViewHelp')
 @endif
+<div class="result-count" data-total="{{ $total }}" data-loaded="{{ $loaded }}">
+    @if ($total === 0)
+        {{ __('projectOverview.result_count_none') }}
+    @elseif ($loaded >= $total)
+        {{ strtr(__('projectOverview.result_count_all'), ['{total}' => $total]) }}
+    @else
+        {{ strtr(__('projectOverview.result_count_partial'), ['{loaded}' => $loaded, '{total}' => $total]) }}
+    @endif
+</div>
 <table class="table table-striped" data-sort-by="{{ $viewSortBy }}"
     data-sort-dir="{{ $viewSortDir }}">
     <thead>
